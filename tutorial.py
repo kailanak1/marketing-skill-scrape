0da1a1757be499fe01bab319f8f176cfe56f9eb9
kailana <kailanakahawaii@gmail.com>
2020-07-17 15:24:27 -0700

    first commit

---
+++ tutorial.py
@@ -0,0 +1,35 @@
+# Selenium Tutorial 
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from bs4 import BeautifulSoup as soup
+import time
+
+url = 'https://www.democracylab.org/index/?section=FindProjects'
+
+wd = webdriver.Chrome()
+wd.get(url)
+
+#wait for dynamic content to load 
+WebDriverWait(wd, 20).until(
+    EC.visibility_of_element_located((By.CLASS_NAME, "ProjectCard-title")))
+
+#grab HTML sourecode 
+html_page = wd.page_source 
+wd.quit()
+
+#parse 
+page = soup(html_page, 'html.parser')
+cards = page.findAll("div", {"class":"ProjectCard-root"})
+
+#loop through the cards
+
+for card in cards: 
+    title = card.find("div", {"class":"ProjectCard-title"})
+
+
+
+
+
