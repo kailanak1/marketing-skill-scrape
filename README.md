5418045560a2b3b5f45881e4f86bec310bd37fdf
kailana <kailanakahawaii@gmail.com>
2020-07-27 12:32:55 -0700

    completed readme

--- README.md
+++ README.md
@@ -3,7 +3,58 @@ Tool to scrape project information from Democracy Lab.
 
 ## Technologies Used 
 Python
-
+Selenium 
 
 ## Status 
-Under construction 
+MVP
+
+## Directions 
+Has not been tested on Windows. 
+
+On a Mac: 
+
+(Install python)[https://www.python.org/downloads/] 
+(Install selenium) [https://selenium-python.readthedocs.io/]
+(Install beautiful soup)[https://www.crummy.com/software/BeautifulSoup/bs4/doc/]
+
+Clone the project on your local machine by forking and cloning this respository. Copy the SSH link, open your terminal, and type:  
+
+```$ git clone <SSH link>```
+
+After cloning the repository, cd into the folder.  
+
+```$ cd selenium-tutorial```
+
+Run the program. 
+
+Scrape all projects: 
+
+```$ python all_projects.py```
+
+Scrape one project: 
+
+```$ python project.py```
+
+The program will scrape all id numbers associated and export title, skills needed, technologies used, and last updated time frame to a csv file. 
+
+## Errors
+Sometimes, the program will fail due to timeouts. If this happens, delete the incomplete csv file and execute the script again. 
+
+
+## License 
+This project is licensed under the [GNU GPL](https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+    Copyright (C) <2020>  <Kailana Kahawaii>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
--- all_projects.py
+++ all_projects.py
@@ -27,63 +27,69 @@ for number in page_number:
 
 
     #wait for dynamic content to load 
-    wait = WebDriverWait(wd, 20)
-    element = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "AboutProjects-description")))
+    try: 
+        wait = WebDriverWait(wd, 20)
+        element = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "AboutProjects-description")))
 
-    #grab HTML sourecode 
-    html_page = wd.page_source 
-    wd.quit()
+        #grab HTML sourecode 
+        html_page = wd.page_source 
+        wd.quit()
 
-    #parse 
-    page = soup(html_page, 'html.parser')
+        #parse 
+        page = soup(html_page, 'html.parser')
 
-    #grab page info
-    results = page.findAll("div", {"class":"AboutProjects-mainColumn"})
+        #grab page info
+        results = page.findAll("div", {"class":"AboutProjects-mainColumn"})
 
-    #about info 
-    about_container = page.findAll("p", {"class":"AboutProjects-icon-text"})
-    last_updated = about_container[1].text
+        #about info 
+        about_container = page.findAll("p", {"class":"AboutProjects-icon-text"})
+        last_updated = about_container[1].text
 
-    #title
-    title_container = results[0].findAll("div", {"class": "AboutProjects-description"})
-    title = title_container[0].h1.text 
+        #title
+        title_container = results[0].findAll("div", {"class": "AboutProjects-description"})
+        title = title_container[0].h1.text 
 
-    #skills 
-    skill_list = [] 
-    if results[0].findAll("div", {"class": "AboutProjects-skills"}):
-        skills_container = results[0].findAll("div", {"class": "AboutProjects-skills"})
-        skills_list = skills_container[0].findAll("p")
-        for skill in skills_list:
-            skill = skill.text
-            skill_list.append(skill)
+        #skills 
+        skill_list = [] 
+        if results[0].findAll("div", {"class": "AboutProjects-skills"}):
+            skills_container = results[0].findAll("div", {"class": "AboutProjects-skills"})
+            skills_list = skills_container[0].findAll("p")
+            for skill in skills_list:
+                skill = skill.text
+                skill_list.append(skill)
 
-    #technologies 
-    tech_list = [] 
-    if results[0].findAll("div", {"class":"AboutProjects-technologies"}):
-        technologies_container = results[0].findAll("div", {"class":"AboutProjects-technologies"})
-        technologies_list = technologies_container[0].findAll("p")
-        for technology in technologies_list: 
-            technology = technology.text 
-            tech_list.append(technology)
+        #technologies 
+        tech_list = [] 
+        if results[0].findAll("div", {"class":"AboutProjects-technologies"}):
+            technologies_container = results[0].findAll("div", {"class":"AboutProjects-technologies"})
+            technologies_list = technologies_container[0].findAll("p")
+            for technology in technologies_list: 
+                technology = technology.text 
+                tech_list.append(technology)
 
-    #cleaning
-    if len(skill_list): 
-        skill_list.remove('Skills Needed')
-        all_skills = '|'.join(skill_list)
-    if len(tech_list):
-        tech_list.remove('Technologies Used')
-        all_tech = '|'.join(tech_list)
+        #cleaning
+        if len(skill_list): 
+            skill_list.remove('Skills Needed')
+            all_skills = '|'.join(skill_list)
+        if len(tech_list):
+            tech_list.remove('Technologies Used')
+            all_tech = '|'.join(tech_list)
 
 
-    #write onto csv file 
-    if not len(skill_list):
-        f.write(title + "," + "" + "," + all_tech + "," + last_updated + "\n")
-    elif not len(tech_list):
-        f.write(title + "," + all_skills + "," + "" + "," + last_updated + "\n")
-    elif not len(skill_list) and not len(tech_list):
-        f.write(title + "," + "" + "," + "" + "," + last_updated + "\n")
-    else:
-        f.write(title + "," + all_skills + "," + all_tech + "," + last_updated + "\n")
+        #write onto csv file 
+        if not len(skill_list):
+            f.write(title + "," + "" + "," + all_tech + "," + last_updated + "\n")
+        elif not len(tech_list):
+            f.write(title + "," + all_skills + "," + "" + "," + last_updated + "\n")
+        elif not len(skill_list) and not len(tech_list):
+            f.write(title + "," + "" + "," + "" + "," + last_updated + "\n")
+        else:
+            f.write(title + "," + all_skills + "," + all_tech + "," + last_updated + "\n")
+
+       
+    except TimeoutException: 
+        print("took too long")
+        break
 f.close()
 
 
